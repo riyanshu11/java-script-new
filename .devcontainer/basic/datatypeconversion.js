cf2b0f749ca02eea28a6412585c736f007177bb9
riyanshu11 <palriyanshu@gmail.com>
2024-08-10 10:25:37 +0000

    container config done

---
+++ .devcontainer/basic/datatypeconversion.js
@@ -0,0 +1,11 @@
+/*let marks=undefined;
+//console.log( typeof (marks));
+let range= Number(marks);
+console.log(range)
+console.log( typeof range)*/
+
+let marks=33;
+//console.log( typeof (marks));
+let range= String(marks);
+console.log(range)
+console.log( typeof range)
