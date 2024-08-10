c8d7f46eb96b0bd26a85683ea9e7bcc850796541
riyanshu11 <palriyanshu@gmail.com>
2024-08-10 09:24:25 +0000

    container config done

---
+++ .devcontainer/basic/datatypes.js
@@ -0,0 +1,10 @@
+let age=18;
+let name="riyanshu";
+let red="fail";
+let you;
+console.log(typeof you)
+console.log(name.length)
+
+let x=100;
+let y=29;
+console.log(x!=y)
---
+++ .devcontainer/basic/day1.js
@@ -0,0 +1,24 @@
+/*
+let first="riyanshu";
+console.log(first);
+let firs="robin"
+console.log(firs)
+
+block scope variable can not be declared again but we can update can also
+*/
+/*
+var first="riyanshu";
+console.log(first);
+var first="robin"
+console.log(first)
+
+global scope variable again declared and updated also
+*/
+/*
+const first="riyanshu";
+console.log(first);
+const first="robin"
+console.log(first)
+
+block scope variable again can not be declared and not  be updated 
+*/
