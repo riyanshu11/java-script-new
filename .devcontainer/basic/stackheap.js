b9e7ccaca5c94f21e997502e6c857b3bdc2dc9f1
riyanshu11 <palriyanshu@gmail.com>
2024-08-13 08:57:39 +0000

    stackheap

---
+++ .devcontainer/basic/stackheap.js
@@ -0,0 +1,18 @@
+// let name="riyanshu";
+// let name1= name;
+// name1="vishal"
+// console.log(name);
+// console.log(name1);
+//  stack type memory
+
+
+let score={
+    vishal:22,
+    neeraj:34,
+    riyanshu:66
+}
+let score1=score;
+
+score1.vishal=33;
+console.log(score.vishla)
+console.log(score1.vishal)
